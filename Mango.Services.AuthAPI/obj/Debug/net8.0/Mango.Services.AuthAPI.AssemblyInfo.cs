64715c52b135f99a64ed777630d5f9872a7dd649
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2023-12-30 21:32:01 +0530

    tere are some issu for discount

--- Mango.Services.AuthAPI/obj/Debug/net8.0/Mango.Services.AuthAPI.AssemblyInfo.cs
+++ Mango.Services.AuthAPI/obj/Debug/net8.0/Mango.Services.AuthAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9646d3950491aa21167873d88475fc363696e2b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+483f0251b04c84d39530bedca3b88035c4571edd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Mango.Services.ShoppingCartAPI/Controllers/CartApiController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartApiController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 
 namespace Mango.Services.ShoppingCartAPI.Controllers
 {
@@ -18,12 +20,14 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         private IMapper _mapper;
         private readonly AppDbContext _db;
         private IProductService _productService;
-        public CartApiController(AppDbContext db, IMapper mapper,IProductService productService)
+        private ICouponService _couponService;
+        public CartApiController(AppDbContext db, IMapper mapper,IProductService productService, ICouponService couponService)
         {
             _db = db;
             _productService = productService;
             this._response = new ResponseDto();
             _mapper = mapper;
+            _couponService = couponService;
         }
 
         [HttpGet("GetCart/{userId}")]
@@ -47,16 +51,15 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
                 }
 
                 //apply coupon if any
-                //if (!string.IsNullOrEmpty(cart.CartHeader.CouponCode))
-                //{
-                //    CouponDto coupon = await _couponService.GetCoupon(cart.CartHeader.CouponCode);
-                //    if (coupon != null && cart.CartHeader.CartTotal > coupon.MinAmount)
-                //    {
-                //        cart.CartHeader.CartTotal -= coupon.DiscountAmount;
-                //        cart.CartHeader.Discount = coupon.DiscountAmount;
-                //    }
-                //}
-
+                if (!string.IsNullOrEmpty(cart.CartHeader.CouponCode))
+                {
+                    CouponDto coupon = await _couponService.GetCoupon(cart.CartHeader.CouponCode);
+                    if (coupon != null && cart.CartHeader.CartTotal > coupon.MinAmount)
+                    {
+                        cart.CartHeader.CartTotal -= coupon.DiscountAmount;
+                        cart.CartHeader.Discount = coupon.DiscountAmount;
+                    }
+                }
                 _response.Result = cart;
             }
             catch (Exception ex)
@@ -67,6 +70,62 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             return _response;
         }
 
+
+        [HttpPost("ApplyCoupon")]
+        public async Task<object> ApplyCoupon([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                var cartFromDb = await _db.CartHeaders.FirstAsync(u => u.UserId == cartDto.CartHeader.UserId);
+                cartFromDb.CouponCode = cartDto.CartHeader.CouponCode;
+                _db.CartHeaders.Update(cartFromDb);
+                await _db.SaveChangesAsync();
+                _response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.ToString();
+            }
+            return _response;
+        }
+
+
+        [HttpPost("RemoveCoupon")]
+        public async Task<object> RemoveCoupon([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                var cartFromDb = await _db.CartHeaders.FirstAsync(u => u.UserId == cartDto.CartHeader.UserId);
+                cartFromDb.CouponCode = "";
+                _db.CartHeaders.Update(cartFromDb);
+                await _db.SaveChangesAsync();
+                _response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.ToString();
+            }
+            return _response;1+
+        }
+
+
+        //[HttpPost("EmailCartRequest")]
+        //public async Task<object> EmailCartRequest([FromBody] CartDto cartDto)
+        //{
+        //    try
+        //    {
+        //        await _messageBus.PublishMessage(cartDto, _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue"));
+        //        _response.Result = true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _response.IsSuccess = false;
+        //        _response.Message = ex.ToString();
+        //    }
+        //    return _response;
+        //}
 
 
         [HttpPost("CartUpsert")]
